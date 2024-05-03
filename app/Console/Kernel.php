1dd8dc6f67108e14baabe54b5e18cc665b2a971e
jose1193 <josegonzalezcr2794@gmail.com>
2024-05-03 23:38:54 +0100

    FIX: Cronjobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('process:transfer')->everyMinute();
+        $schedule->command('process:transfer')->daily();
     }
 
     /**
--- app/Http/Livewire/ProcessOperationExpense.php
+++ app/Http/Livewire/ProcessOperationExpense.php
@@ -468,7 +468,7 @@ private function processTodayOrFutureOperation($validatedData)
 
         // Asignar subcategoría y actualizar ingreso presupuestario
         $this->ProcessSubcategoryOperationAssignment($processOperation);
-        $this->ProcessBudgetIncome($validatedData['budget_id'] ?? null, $processOperation);
+        $this->ProcessBudgetExpense($validatedData['budget_id'] ?? null, $processOperation);
         }  else {
             // Si es una nueva operación en ProcessOperation
             $processOperation = ProcessOperation::create($validatedData);
